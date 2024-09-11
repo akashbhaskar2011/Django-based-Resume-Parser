3c5b9c8cd5ce18be64daa65b5fab6600e3756772
akashbhaskar2011 <akashbhaskar.2011@gmail.com>
2024-09-12 04:31:39 +0530

    chagned database to postgres

--- ResumeProcessor/settings.py
+++ ResumeProcessor/settings.py
@@ -75,13 +75,27 @@ WSGI_APPLICATION = 'ResumeProcessor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'b7cdIxqwSB0k',
+        'HOST': 'ep-delicate-queen-a1oxnzxn-pooler.ap-southeast-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',  # Ensure SSL is used
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
